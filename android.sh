c468a5dd0e3eeef1e665c1b908ed079a65619e5c
Oğuz Kağan EREN <me@oguzkaganeren.com.tr>
2019-12-01 13:32:35 +0300

    Create android.sh

---
+++ android.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+# Author 	: Oğuz Kağan EREN
+# Website : https://www.oguzeren.com.tr
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+
+cd /run/media/oguz/Files/Programs/sdkLinux/emulator
+./emulator -avd Pixel_2_API_29
